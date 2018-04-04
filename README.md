566f23701da0976bb57b47cf97e5db850f16b998
Michael Bolens <bolensmichael@gmail.com>
2018-04-04 08:31:58 -0600

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gddns-updater
 Google Domains Dynamic DNS Updater
+
+## Install instructions
+- `npm i`
+- Add `.env` file with your domain info, for example:
+```
+GDDNS_USER=YOUR_HOST_USERNAME
+GDDNS_PASS=YOUR_HOST_PASSWORD
+GDDNS_HOST=YOUR_HOST
+```
+- Run `node app.js`
---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+require('dotenv').config();
+const extIP = require('external-ip');
+const request = require("request");
+const userAgent = 'GDDNS Updater';
+
+let getIP = extIP({
+  replace: true,
+  services: ['http://ifconfig.co/x-real-ip', 'http://ifconfig.io/ip'],
+  timeout: 600,
+  getIP: 'parallel',
+  userAgent: userAgent
+});
+
+getIP((err, ip) => {
+  if (err) {
+    throw err;
+  }
+  console.log(ip);
+  updateIP(ip);
+});
+
+function updateIP(ip) {
+  let options = {
+    method: 'POST',
+    url: 'https://' + process.env.GDDNS_USER + ':' + process.env.GDDNS_PASS + '@domains.google.com/nic/update',
+    qs: {
+      hostname: process.env.GDDNS_HOST,
+      myip: ip
+    },
+    headers: {
+      'User-Agent': userAgent
+    }
+  };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
